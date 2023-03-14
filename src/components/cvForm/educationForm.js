b943f54d6feb00db16ea701453b88df91ef75fd3
gcamaci <77690094+gcamaci@users.noreply.github.com>
2023-03-13 21:01:42 -0500

    elements not appending dynamicly

--- src/components/cvForm/educationForm.js
+++ src/components/cvForm/educationForm.js
@@ -3,14 +3,30 @@ import React, {Component} from "react";
 class Education extends Component{
     constructor(){
         super()
+
+        this.renderEducationForm = this.renderEducationForm.bind(this)
     }
 
 
+    renderEducationForm = () => {
+        console.log('working')
+        return(
+            <div>
+                <label>School</label>
+                <input type='text'></input>
+                <label>Degree</label>
+                <input type='text'></input>
+                <label>Description</label>
+                <input type='text'></input>
+            </div>
+
+        )
+    }
     render(){
         return(
             <div>
                 <h1>Education</h1>
-                <button type="button">+</button>
+                <button onClick={this.renderEducationForm} type="button">+</button>
             </div>
         )
     }
